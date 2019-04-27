6e9e4553aa7bb377846f70156767f29a1cba019f
DieseKartoffel <diesekartoffel@gmx.de>
2019-04-28 00:25:27 +0200

    config.py can now be edited without ever being commited

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 #Place yoyr OAuth2 Dropbox access token here
+#used following command to never stage this file after editing:
+#git update-index --assume-unchanged "config.py"
+#nd to undo that git update-index --no-assume-unchanged "config.py"
 TOKEN = "YOUR_TOKEN_SECRET"
