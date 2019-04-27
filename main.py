ef1182eb312d4fae570f76f91175e5fa96097858
DieseKartoffel <diesekartoffel@gmx.de>
2019-04-27 17:01:48 +0100

    listing usb devices

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
-import _thread
-import pyudev
+import sys
+import usb.core
+import usb.backend.libusb1
 
-class USBDetector():
-    ''' Monitor udev for detection of usb '''
- 
-    def __init__(self):
-        self._listen()
-
-    def _listen(self):
-        while True:
-            ''' Runs the actual loop to detect the events '''
-            self.context = pyudev.Context()
-            self.monitor = pyudev.Monitor.from_netlink(self.context)
-            self.monitor.filter_by(subsystem='usb')
-            # this is module level logger, can be ignored
-            LOGGER.info("Starting to monitor for usb")
-            self.monitor.start()
-            for device in iter(self.monitor.poll, None):
-                LOGGER.info("Got USB event: %s", device.action)
-                if device.action == 'add':
-                    # some function to run on insertion of usb
-                    self.on_created()
-                else:
-                    # some function to run on removal of usb
-                    self.on_deleted()
-
-                
-if __name__ == '__main__':
-    print("This only executes when %s is executed rather than imported" % __file__)
-    usbd = USBDetector()
+backend = usb.backend.libusb1.get_backend(find_library=lambda x: "./libusb-1.0.20.7/MS64/dll/libusb-1.0.dll")
+if not backend:
+    print("bye")
+dev = usb.core.find(backend=backend)
+# find USB devices
+dev = usb.core.find(find_all=True)
+# loop through devices, printing vendor and product ids in decimal and hex
+for cfg in dev:
+  sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
+  sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
