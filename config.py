0c68afa5fb1a2c5694c01279605966d549f63b37
DieseKartoffel <diesekartoffel@gmx.de>
2019-04-28 02:03:53 +0200

    Update config.py
    new params from main now in config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 #Place yoyr OAuth2 Dropbox access token here
+TOKEN = "jZGXXXXXXXX"
+USB_FOLDER = "Dropbox"          #name of the folder that is synchronized from the new drive to Dropbox
+DROPBOX_FOLDER = "USB_Drive"    #name of the folder that Dropbox contains
+REFRESH_TIME = 5;               #time in seconds to wait between scanning for a new drive
+
+#For git:
 #used following command to never stage this file after editing:
 #git update-index --assume-unchanged "config.py"
-#nd to undo that git update-index --no-assume-unchanged "config.py"
-TOKEN = "YOUR_TOKEN_SECRET"
+#undo with: git update-index --no-assume-unchanged "config.py"
